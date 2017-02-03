133afe54c5060d042ab5c290f1f12e4aebc8e32d
Enes YILMAZ <enisyilmaz12@gmail.com>
2017-02-03 19:43:55 +0300

    Changed examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,8 @@ var React = require('react');
 var Examples = (props) => {
 	return (
 		<div>
-			<h3>Examples Component</h3>
+			<h3>Examples</h3>
+			<p>Welcome to examples page!</p>
 		</div>		
 	);
 };
--- public/bundle.js
+++ public/bundle.js
@@ -26602,7 +26602,12 @@
 			React.createElement(
 				'h3',
 				null,
-				'Examples Component'
+				'Examples'
+			),
+			React.createElement(
+				'p',
+				null,
+				'Welcome to examples page!'
 			)
 		);
 	};
