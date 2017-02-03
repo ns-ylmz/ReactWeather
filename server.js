5e4eb1ab033d0b8eff3afc6281b5e40bed92c415
Enes YILMAZ <enisyilmaz12@gmail.com>
2017-02-03 19:56:15 +0300

    Updated middleware for localhost

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-	if(req.headers['x-forwarded-proto'] === 'http') {
-		next();
-	} else {
+	if(req.headers['x-forwarded-proto'] === 'https') {
 		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
 	}
 });
 
