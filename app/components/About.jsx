a594a270155787c294973f5988715d66314ec482
Enes YILMAZ <enisyilmaz12@gmail.com>
2017-02-03 21:53:12 +0300

    Style about page and link to resource

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,23 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page.</p>
+			<h1 className="text-center">About</h1>
+			<p>This is weather application build on React.I have built this for something.</p>
+			<p>Here are some of the tools I used:</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">
+						React
+					</a>
+					 - This was the Javascript framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">
+						Open Weather Map
+					</a> 
+					- I used Open Weather Map to search for weather data by city name.
+				</li>
+			</ul>
 		</div>
 	);
 };
--- public/bundle.js
+++ public/bundle.js
@@ -26680,23 +26680,52 @@
 /* 254 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var About = function About(props) {
 		return React.createElement(
-			'div',
+			"div",
 			null,
 			React.createElement(
-				'h3',
-				null,
-				'About'
+				"h1",
+				{ className: "text-center" },
+				"About"
 			),
 			React.createElement(
-				'p',
+				"p",
 				null,
-				'Welcome to the about page.'
+				"This is weather application build on React.I have built this for something."
+			),
+			React.createElement(
+				"p",
+				null,
+				"Here are some of the tools I used:"
+			),
+			React.createElement(
+				"ul",
+				null,
+				React.createElement(
+					"li",
+					null,
+					React.createElement(
+						"a",
+						{ href: "https://facebook.github.io/react" },
+						"React"
+					),
+					"- This was the Javascript framework used."
+				),
+				React.createElement(
+					"li",
+					null,
+					React.createElement(
+						"a",
+						{ href: "http://openweathermap.org" },
+						"Open Weather Map"
+					),
+					"- I used Open Weather Map to search for weather data by city name."
+				)
 			)
 		);
 	};
